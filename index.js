cc4d77dc6df990dba2adf21bc36b507518f64857
jutaeho <dramavoy@daum.net>
2020-07-07 21:06:44 +0900

    install react

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ let app = express();
 
 let port = process.env.PORT || 4000;
 
+// set view
+app.set('views', path.join(__dirname, 'dist'));
+app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
 app.get('/', (req, res) => {
-    res.end('hello react redux');
+    res.render(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`starting server port on ${port}`);
